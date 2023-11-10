f1fd6ba1ac37ff3c30c8bfa8fcd5c838654107aa
oktamov <mr006developer@gmail.com>
2023-11-10 15:17:19 +0500

    chat-created

---
+++ chat/admin.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
+from .models import MessageModel, DialogsModel
+
+
+class MessageModelAdmin(ModelAdmin):
+    readonly_fields = ('created', 'modified',)
+    search_fields = ('id', 'text', 'sender__pk', 'recipient__pk')
+    list_display = ('id', 'sender', 'recipient', 'text', 'file', 'read')
+    list_display_links = ('id',)
+    list_filter = ('sender', 'recipient')
+    date_hierarchy = 'created_at'
+
+
+class DialogsModelAdmin(ModelAdmin):
+    readonly_fields = ('created', 'modified',)
+    search_fields = ('id', 'user1__pk', 'user2__pk')
+    list_display = ('id', 'user1', 'user2')
+    list_display_links = ('id',)
+    date_hierarchy = 'created_at'
+
+
+admin.site.register(DialogsModel)
+admin.site.register(MessageModel)
+
+# site.register(DialogsModel, DialogsModelAdmin)
+# site.register(MessageModel, MessageModelAdmin)
